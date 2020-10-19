6d79348c1636357d7c0e124f746d8ed74a68ecaf
jjuny0113 <68732435+jjuny0113@users.noreply.github.com>
2020-10-18 23:35:35 -0700

    JS:Navbar menu click

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+// 지정된 id부분으로 스크롤링
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
