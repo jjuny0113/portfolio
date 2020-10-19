b79c6849f27918303e570bce889156b06af2eb50
jjuny0113 <68732435+jjuny0113@users.noreply.github.com>
2020-10-19 01:35:28 -0700

    JS Home transparent

--- index.js
+++ index.js
@@ -34,3 +34,11 @@ const contact = document.querySelector(".home__contact");
 contact.addEventListener("click", (event) => {
   scrollIntoView("#contact");
 });
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  console.log(1 - window.scrollY / homeHeight);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
