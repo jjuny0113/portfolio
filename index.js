8b22415b298ab7a9979e665a2badd16dd8f526ad
jjuny0113 <68732435+jjuny0113@users.noreply.github.com>
2020-10-20 00:18:31 -0700

    JS: work filtering animation

--- index.js
+++ index.js
@@ -69,12 +69,17 @@ workBtnContainer.addEventListener("click", (e) => {
     return;
   }
 
-  projects.forEach((project) => {
-    console.log(project.dataset.type);
-    if (filter === "*" || filter === project.dataset.type) {
-      project.classList.remove("invisible");
-    } else {
-      project.classList.add("invisible");
-    }
-  });
+  projectContainer.classList.add("anim-out");
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
 });
